af3522529627aae5ef5ec117f68d9a287287e534
Abdulrahman_Ka <abode.kalls@gmail.com>
2026-01-04 10:10:54 +0300

    show lines in trending 24h

--- components/home/TrendingCoins.tsx
+++ components/home/TrendingCoins.tsx
@@ -1,8 +1,10 @@
 import { fetcher } from "@/lib/coingecko.action";
 import DataTable from "../DataTable";
-import { formatCurrency } from "@/lib/utils";
+import { cn, formatCurrency, formatPercentage } from "@/lib/utils";
 import Image from "next/image";
 import { TrendingCoinsFallback } from "./fallback";
+import Link from "next/link";
+import { TrendingDown, TrendingUp } from "lucide-react";
 
 const TrendingCoins = async () => {
   let trendingCoins;
@@ -21,29 +23,46 @@ const TrendingCoins = async () => {
     {
       header: "Name",
       cellClassName: "name-cell",
-      cell: (coin) => (
-        <>
-          <Image
-            src={coin.item.large}
-            alt={coin.item.name}
-            width={32}
-            height={32}
-            className="name-image"
-          />
-          <p>{coin.item.name}</p>
-        </>
-      ),
+      cell: (coin) => {
+        const item = coin.item;
+        return (
+          <Link href={`/coins/${item.id}`}>
+            <Image src={item.large} alt={item.name} width={36} height={36} />
+            <p>{item.name}</p>
+          </Link>
+        );
+      },
     },
     {
       header: "24h Changes",
       cellClassName: "change-cell",
-      cell: (coin) => (
-        <p className="change-line">
-          {coin.item.data.price_change_percentage_24h?.usd
-            ? `${coin.item.data.price_change_percentage_24h.usd.toFixed(2)}%`
-            : "N/A"}
-        </p>
-      ),
+      cell: (coin) => {
+        const item = coin.item;
+        const isTrendingUp = item.data.price_change_percentage_24h.usd > 0;
+
+        return (
+          <div
+            className={cn(
+              "price-change",
+              isTrendingUp ? "text-green-500" : "text-red-500"
+            )}
+          >
+            <p className="flex items-center">
+              {formatPercentage(item.data.price_change_percentage_24h.usd)}
+              {isTrendingUp ? (
+                <TrendingUp width={16} height={16} />
+              ) : (
+                <TrendingDown width={16} height={16} />
+              )}
+            </p>
+          </div>
+        );
+        // <p className="change-line">
+        //   {coin.item.data.price_change_percentage_24h?.usd
+        //     ? `${coin.item.data.price_change_percentage_24h.usd.toFixed(2)}%`
+        //     : "N/A"}
+        // </p>
+      },
     },
     {
       header: "Price",
